80d12f28dc8239260b5d93de57b6cb227067f7c3
mueid22110 <mueidhasan9@gmail.com>
2024-11-02 00:37:30 +0600

    Assignment 5 modified

--- Assignment_05.java
+++ Assignment_05.java
@@ -7,32 +7,11 @@ public class Assignment_05 {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
-        switch(x){
-            case 1:
-                System.out.println(Day[x-1]);
-                break;
-            case 2:
-                System.out.println(Day[x-1]);
-                break;
-            case 3:
-                System.out.println(Day[x-1]);
-                break;
-            case 4:
-                System.out.println(Day[x-1]);
-                break;
-            case 5:
-                System.out.println(Day[x-1]);
-                break;
-            case 6:
-                System.out.println(Day[x-1]);
-                break;
-            case 7:
-                System.out.println(Day[x-1]);
-                break;
-            default:
-                myFunc();
+        if(x>0 && x<8) {
+            System.out.println(Day[x - 1]);
+        }else myFunc();
 
-        }
+
     }
     public static void main(String[] args) {
         myFunc();
